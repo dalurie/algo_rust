7d7502a18117fb58faf782105dccfb7fecc29353
dalurie <45216937+dalurie@users.noreply.github.com>
2020-04-24 15:22:19 -0700

    Add files via upload
    bin to lib style;
    print statements -> tests;

--- quicksort.rs
+++ quicksort.rs
@@ -1,16 +1,7 @@
-fn main() {
-    let x:Vec<i32> = vec![1, 0, -32, 3, 51];
-    let y:Vec<f64> = vec![1.0, 0.0, -32.0, 3.0, 51.0];
-    let z:Vec<&str> = vec!["world", "hello"];
-    println!("quicksort({:?}): = {:?}", x, quicksort(&x));
-    println!("quicksort({:?}): = {:?}", y, quicksort(&y));
-    println!("quicksort({:?}): = {:?}", z, quicksort(&z));
-}
-
-fn quicksort<T: Clone + PartialOrd>(x: &Vec<T>) -> Vec<T> {
-    let smaller: Vec<T> = x.iter().filter(|i| i < &&x[0]).map(|i| i.clone()).collect();
-    let equal:   Vec<T> = x.iter().filter(|i| i == &&x[0]).map(|i| i.clone()).collect();
-    let greater: Vec<T> = x.iter().filter(|i| i > &&x[0]).map(|i| i.clone()).collect();
+pub fn quicksort<T: Clone + PartialOrd>(x: &Vec<T>) -> Vec<T> {
+    let smaller: Vec<T> = x.iter().filter(|i| **i < x[0]).map(|i| i.clone()).collect();
+    let equal:   Vec<T> = x.iter().filter(|i| **i == x[0]).map(|i| i.clone()).collect();
+    let greater: Vec<T> = x.iter().filter(|i| **i > x[0]).map(|i| i.clone()).collect();
     if x.len() == 0 {
         vec![]
     } else if x.len() == 1 {
@@ -18,4 +9,17 @@ fn quicksort<T: Clone + PartialOrd>(x: &Vec<T>) -> Vec<T> {
     } else {
         [quicksort(&smaller), quicksort(&equal), quicksort(&greater)].concat()
     }
+}
+
+#[test]
+fn neg_check() {
+    assert_eq!(quicksort(&vec![1, 0, -32, 3, 51]), vec![-32, 0, 1, 3, 51]);
+}
+#[test]
+fn float_check() {
+    assert_eq!(quicksort(&vec![1.0, 0.0, -32.0, 3.0, 51.0]), vec![-32.0, 0.0, 1.0, 3.0, 51.0]);
+}
+#[test]
+fn str_check() {
+    assert_eq!(quicksort(&vec!["world", "hello"]), vec!["hello","world"]);
 }
